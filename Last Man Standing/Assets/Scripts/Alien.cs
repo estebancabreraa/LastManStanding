2176b367d20cdbc68339414c3121ec64bb3493f8
her17138 <her17138@uvg.edu.gt>
2018-02-13 23:28:08 -0600

    creacion de todo
    Creacion de personaje, background, algunos enemigos y condiciones de muerte :)

---
+++ Last Man Standing/Assets/Scripts/Alien.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Alien : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+    SpriteRenderer sr;
+	Animator anim;
+	AudioSource audio;
+    public Camera cam;
+    private float speed = 5f;
+    private float jumpForce = 250f;
+    private bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+		anim = GetComponent<Animator> ();
+		audio = GetComponent<AudioSource> ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float move = Input.GetAxis("Horizontal");
+        if (move != 0) {
+            rb2d.transform.Translate(new Vector3(1, 0, 0) * move * speed * Time.deltaTime);
+            cam.transform.position = new Vector3(rb2d.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+            facingRight = move > 0;
+        }
+        
+        sr.flipX = !facingRight;
+
+        if (Input.GetButtonDown("Jump")) {
+            rb2d.AddForce(Vector2.up*jumpForce);
+			audio.Play ();
+        }
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+	}
+    private void OnTriggerEnter2D (Collider2D collision)
+	{
+		GameController.instance.gameOver = true;
+
+	}
+}
